9aa66f103a3f33f193acf9a12c03a570cc99e3d6
gillsinaglass <rdgill95@gmail.com>
2019-02-21 17:19:26 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+const token = ' b505de8bd9267637dcfc86dbe16bc37e6e7338ab';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
