af49d0a86d49e05fdae45ca994f5ac51682e7017
dcjones1 <dcejjones@gmail.com>
2019-02-21 16:10:30 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
+require('dotenv').config()
+
 const app = "I don't do much.";
+
+const token = GIT_KEY
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+.then(res => res.json())
+.then(json => console.log(json))
