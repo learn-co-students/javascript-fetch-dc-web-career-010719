eaa3a99508abe96e1274c8f80e1bd1bf5685032d
Ryan Gill <37309520+gillsinaglass@users.noreply.github.com>
2019-02-22 10:47:33 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much.";
-const token = ' b505de8bd9267637dcfc86dbe16bc37e6e7338ab';
+
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
