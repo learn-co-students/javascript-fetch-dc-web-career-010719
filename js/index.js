ba8f3a7d2700f19581dd8c1ef82e3f959769b20a
Serven Maraghi <maraghiserven@gmail.com>
2019-02-21 22:33:52 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+const token = ''
+ 
+fetch('https://github.com/learn-co-curriculum/javascript-fetch-lab', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
