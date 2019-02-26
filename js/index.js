6024de1fdda6583ab3083360d29cb138a15fda9c
Dusty211 <kylebenz@hotmail.com>
2019-02-26 08:10:35 -0500

    finished

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-const app = "I don't do much.";
+const token = 'Haha!! You will NEVER get my TOOKKKKEEEENNN!!!!!';
+fetch('https://api.github.com/users/Dusty211/repos',
+{ headers: { Authorization: `token ${token}` }  })
+.then(res => res.json())
+.then(json => console.log(json));
